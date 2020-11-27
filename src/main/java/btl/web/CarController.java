46218d9866364fdd2810459bf7c05669d6db4e39
Thinh <thinh.opentechiz@gmail.com>
2020-11-27 10:12:26 +0700

    1 add car

--- src/main/java/btl/web/CarController.java
+++ src/main/java/btl/web/CarController.java
@@ -1,13 +1,18 @@
 package btl.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import btl.Car;
@@ -16,7 +21,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-@RequestMapping("/") 
+@RequestMapping("/car") 
 public class CarController {
 	private final CarRepository carRepo;
 	
@@ -25,23 +30,54 @@ public class CarController {
 		this.carRepo = carRepo;
 	} 
 	
-	@ModelAttribute
-	public void addIngredientsToModel(Model model) {
-		
-	}
-	
 	@GetMapping
-	public String showAddForm(Model model) {
+	public String showAllCar(Model model) {
 		List<Car> car = (List<Car>) carRepo.findAll(); 
 		model.addAttribute("cars", car);
 		return "findAll";
 	}
-	
-	@PostMapping
-	public String addIngredient(Car car, Model model) {
+	@GetMapping("/add")
+	public String showAddForm(Model model) {
+		model.addAttribute("car", new Car());
+		return "add";
+	}
+	@PostMapping("/add")
+	public String addCar(Car car, Model model) {
 		carRepo.save(car);
 		model.addAttribute(car);
 //		log.info("Ingredient saved: " + car);
-		return "addIngredientSuccess";
+//		return "addCarSuccess";
+		return "redirect:/car";
+	}
+	@GetMapping("/edit/{id}")
+	public String showEditForm(Model model) {
+		model.addAttribute("car", new Car());
+		return "edit";
+	}
+	@PutMapping("/edit/{id}")
+	public String editCar(@PathVariable("id") Long id, @RequestBody Car carDetail, Model model) {
+		Optional<Car> car = carRepo.findById(id);
+		
+		carRepo.save(carDetail);
+		model.addAttribute(car);
+//		log.info("Ingredient saved: " + car);
+//		return "addCarSuccess";
+		return "redirect:/car";
+	}
+	
+	@GetMapping("/{id}")
+	public Car carById(@PathVariable("id") Long id) {
+	Optional<Car> optCar = carRepo.findById(id);
+			if (optCar.isPresent()) {
+			return optCar.get();
+			}
+		return null;
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteCar(@PathVariable("id") Long id, Car car,Model model) {
+		carRepo.deleteById(id);
+		model.addAttribute(car);
+		return "redirect:/car";
 	}
 }
